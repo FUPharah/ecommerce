d9e08f55cf43e9c10b9d2714255ecc691a5d7898
canozben <canozben@windowslive.com>
2023-08-09 15:08:19 +0300

    size form redo values can be selected from a dropdown instead of manual writing

--- app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/components/size-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/components/size-form.tsx
@@ -14,6 +14,7 @@ import { toast } from "react-hot-toast";
 import axios from "axios";
 import { useParams, useRouter } from "next/navigation";
 import { AlertModal } from "@/components/modals/alert-modal";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 
 interface SizeFormProps {
@@ -24,6 +25,16 @@ const formSchema = z.object({
   name: z.string().min(1).max(255),
   value: z.string().min(1).max(255),
 });
+
+const sizes = [
+  { name: "Extra Small", value: "XS" },
+  { name: "Small", value: "S" },
+  { name: "Medium", value: "M" },
+  { name: "Large", value: "L" },
+  { name: "Extra Large", value: "XL" },
+  { name: "2X Large", value: "XXL" },
+  { name: "3X Large", value: "XXXL" },
+]
 
 type SizeFormValues = z.infer<typeof formSchema>;
 
@@ -55,14 +66,17 @@ export const SizeForm: React.FC<SizeFormProps> = ({
     router.back();
   }
 
-  const onSubmit = async (data: SizeFormValues) => {
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+
     try {
       setLoading(true);
 
+      const formData = form.getValues(); // Get the form data from the react-hook-form
       if (initialData) {
-        await axios.patch(`/api/${params.storeId}/sizes/${params.sizeId}`, data);
+        await axios.patch(`/api/${params.storeId}/sizes/${params.sizeId}`, formData);
       } else {
-        await axios.post(`/api/${params.storeId}/sizes`, data);
+        await axios.post(`/api/${params.storeId}/sizes`, formData);
       }
 
       router.refresh();
@@ -74,6 +88,7 @@ export const SizeForm: React.FC<SizeFormProps> = ({
       setLoading(false);
     }
   };
+
 
   const onDelete = async () => {
     try {
@@ -91,55 +106,70 @@ export const SizeForm: React.FC<SizeFormProps> = ({
   };
 
   return (
-  <>
+    <>
     <AlertModal
-    isOpen={open}
-    onClose={() => setOpen(false)}
-    onConfirm={onDelete}
-    loading={loading}
+      isOpen={open}
+      onClose={() => setOpen(false)}
+      onConfirm={onDelete}
+      loading={loading}
     />
     <div className="flex items-center justify-between">
-      <Heading title= {title} description= {description} />
-      {initialData && (
-      <Button disabled={loading} variant="destructive" className="bg-gradient-to-r
-        from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4
-        focus:outline-none focus:ring-red-300 dark:focus:ring-red-800
-        shadow-lg shadow-red-500/50 dark:shadow-lg
-        dark:shadow-red-800/80"
-        size="icon"
-        onClick={() => setOpen(true)}
-      >
-        <Trash2 className="h-4 w-4 "/>
-      </Button>
-      )}
-    </div>
-    <Separator/>
+        <Heading title={title} description={description} />
+        {initialData && (
+          <Button
+            className="bg-gradient-to-r border-2 border-red-300 hover:border-red-900
+          from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4
+            focus:outline-none focus:ring-red-300 dark:focus:ring-red-800
+            shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80"
+            disabled={loading} variant="destructive" type="button" size="icon" onClick={() => setOpen(true)}>
+          <Trash2 className="h-4 w-4 hover:text-black"/>
+          </Button>
+        )}
+      </div>
+      <Separator />
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
-        <div className="grid grid-cols-3 gap-8">
-          <FormField control={form.control} name="name" render={({ field }) => (
-            <FormItem>
-              <FormLabel>
-                <span className="text-sm font-medium text-gray-700">Name</span>
-              </FormLabel>
-              <FormControl>
-                <Input disabled={loading} placeholder="Size Name" {...field}/>
-              </FormControl>
-              <FormMessage/>
-            </FormItem>
-          )}/>
-          <FormField control={form.control} name="value" render={({ field }) => (
+      <form onSubmit={onSubmit} className="space-y-8 w-full">
+        <div className="md:grid md:grid-cols-4 gap-8">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl className="border-2 border-black hover:border-sky-500">
+                  <Input disabled={loading} placeholder="Size Name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}/>
+            <FormField control={form.control} name="value" render={({ field }) => (
             <FormItem>
               <FormLabel>
                 <span className="text-sm font-medium text-gray-700">Value</span>
               </FormLabel>
+            <Select disabled={loading} onValueChange={field.onChange} value={field.value} defaultValue={field.value}>
               <FormControl>
-                <Input disabled={loading} placeholder="Size Value" {...field}/>
+                <SelectTrigger className="border-2 border-black hover:border-sky-500" >
+                  <SelectValue defaultValue={field.value} placeholder="Select a billboard" />
+                </SelectTrigger>
               </FormControl>
-              <FormMessage/>
-            </FormItem>
-          )}/>
-        </div>
+              <SelectContent >
+                {sizes.map((size) => (
+                  <SelectItem key={size.value} value={size.name}>{size.value}
+                    {size.name === field.value && (
+                      <span className="text-xs text-gray-500 dark:text-gray-400 ml-2">(Current)</span>
+                    )}
+                    {size.value == field.value && (
+                      <span className="text-sm text-gray-400 ml-1"> (Current)</span>
+                    )}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          <FormMessage />
+        </FormItem>
+        )}/>
+          </div>
         <div className="flex items-center justify-between">
         <div>
         <Button disabled={loading} className="text-white bg-gradient-to-r
@@ -148,7 +178,8 @@ export const SizeForm: React.FC<SizeFormProps> = ({
           rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 border-green-500 border-2
           border-opacity-50 hover:border-lime-800 focus:border-lime-200
           shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80"
-          type="submit">
+          type="submit"
+          >
           <span className="font-bold">{action}</span>
         </Button>
         </div>
@@ -164,7 +195,7 @@ export const SizeForm: React.FC<SizeFormProps> = ({
             onClick={onCancel} type="button">
             <span className="font-bold hover:text-black">Cancel</span>
           </Button>
-        </div>
+          </div>
         </div>
       </form>
     </Form>
