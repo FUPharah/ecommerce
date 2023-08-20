b4e4a3354cabf765819f09974728f37ef7c068a4
canozben <canozben@windowslive.com>
2023-08-20 05:26:32 +0300

    Dark and Light mode added

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import { MainNav } from "./main-nav";
 import StoreSwitcher from "./store-switcher";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import { ModeToggle } from "./ui/mode-toggle";
 
 const Navbar = async () => {
   const { userId } = auth();
@@ -17,9 +18,10 @@ const Navbar = async () => {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <StoreSwitcher className="bg-stone-300" items={stores}/>
+        <StoreSwitcher items={stores}/>
         <MainNav className="mx-6"/>
         <div className="ml-auto flex items-center space-x-4">
+          <ModeToggle/>
           <div className="border-2 rounded-full border-rose-600">
           <UserButton afterSignOutUrl="/"/>
           </div>
